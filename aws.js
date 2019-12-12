df1bd4a6683f3fa4c6dfed0d9b06f94b43b1d6ef
Tyler Suggs <tylersuggs@users.noreply.github.com>
2019-12-12 11:10:33 -0500

    Update aws.js

--- aws.js
+++ aws.js
@@ -2,6 +2,15 @@ function onError(error) {
   console.log(`Error: ${error}`);
 }
 
+function getForegroundColors(bgColor) {
+  var fgColors[];
+  
+  fgColors[0] = (bgColor.r * 299 + bgColor.g * 587 + bgColor.b * 114) / 1000 > 125 ? '#000000' : '#FFFFFF';
+  fgColors[1] = fgColor[0] == '#000000' ? '#CCCCCC' : '#000000';
+  
+  return fgColors
+}
+
 function onGot(item) {
   var acct = document.getElementById("awsc-login-display-name-account")
     .textContent;
@@ -12,6 +21,16 @@ function onGot(item) {
       )
       .forEach(function(ele) {
         ele.style.backgroundColor = item[acct];
+      });
+    
+    var foreground = getForgroundColors(item[acct]);
+    
+    document
+      .querySelectorAll(
+        "body #awsgnav #nav-menubar"
+      )
+      .forEach(function(ele) {
+        ele.style.textShadow.color = foreground[0];
       });
   }
 }
